ad12b9c849a3da30daf5e39c01fea402436ddcea
Maxim <Maxzb86@gmail.com>
2025-02-14 00:48:06 +0300

    architecture: separate theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'package:bitcoin/routes/routes.dart';
+import 'package:bitcoin/theme/theme.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -25,33 +26,7 @@ class CryptoListApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'CryptoApp',
-      theme: ThemeData(
-        appBarTheme: AppBarTheme(
-          backgroundColor: Colors.blueGrey,
-          titleTextStyle: TextStyle(
-            color: Colors.white,
-            fontSize: 22,
-            fontWeight: FontWeight.w500,
-          ),
-        ),
-        scaffoldBackgroundColor: Colors.blueGrey,
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.amber),
-        listTileTheme: ListTileThemeData(iconColor: Colors.white70),
-        useMaterial3: true,
-        // Переопределяем встроенные MaterialUI стили
-        // например может переопределить только цвет для titleMedium
-        textTheme: TextTheme(
-          titleMedium: TextStyle(
-            color: Colors.white,
-          ),
-          titleSmall: TextStyle(
-            color: Colors.white70,
-          ),
-          bodySmall: TextStyle(
-            color: Colors.white70,
-          ),
-        ),
-      ),
+      theme: darkTheme,
       routes: routes,
       // Убираем home так как определили стартовый экран выше с помощью '/'
       // home: const CryptoListScreen(),
---
+++ lib/theme/theme.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+final darkTheme = ThemeData(
+  appBarTheme: AppBarTheme(
+    backgroundColor: Colors.blueGrey,
+    titleTextStyle: TextStyle(
+      color: Colors.white,
+      fontSize: 22,
+      fontWeight: FontWeight.w500,
+    ),
+  ),
+  scaffoldBackgroundColor: Colors.blueGrey,
+  colorScheme: ColorScheme.fromSeed(seedColor: Colors.amber),
+  listTileTheme: ListTileThemeData(iconColor: Colors.white70),
+  useMaterial3: true,
+  // Переопределяем встроенные MaterialUI стили
+  // например может переопределить только цвет для titleMedium
+  textTheme: TextTheme(
+    titleMedium: TextStyle(
+      color: Colors.white,
+    ),
+    titleSmall: TextStyle(
+      color: Colors.white70,
+    ),
+    bodySmall: TextStyle(
+      color: Colors.white70,
+    ),
+  ),
+);
